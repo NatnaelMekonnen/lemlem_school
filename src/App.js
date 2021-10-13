ce30da7a403d33808974402f02d365089c2b39e2
Natnael Mekonnen <nhattymack204@gmail.com>
2021-10-13 12:22:03 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from '@mui/material';
+import React from 'react';
+import Logo from '../Images/Logo.svg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='TopBar'>
+        <div>
+          <img src={Logo} alt='LS' />
+          <Typography>Lemlem School</Typography>
+        </div>
+      </div>
+      <div className='LandingPage'>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
